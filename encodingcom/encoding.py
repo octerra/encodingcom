0d5bf910f384414360c9fe76a85ea5d945f42913
David Hwu <dhwu@studionow.com>
2015-02-04 08:20:28 -0800

    Fix all the warnings.  2. Pylint score dramatically improved: 7.71/10 3. PEP8 compliance

--- encodingcom/encoding.py
+++ encodingcom/encoding.py
@@ -3,69 +3,118 @@ import urllib.parse
 import json
 
 ENCODING_API_URL = 'manage.encoding.com:80'
-ENCODING_API_HEADERS = {'Content-Type':'application/x-www-form-urlencoded'}
+ENCODING_API_HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
+
 
 class Encoding(object):
+    """
+    Helper class to talk to Encoding.com server
 
-    def __init__(self, userid, userkey, url=ENCODING_API_URL):
+    """
+
+    def __init__(self, user_id, user_key, url=ENCODING_API_URL):
         self.url = url
-        self.userid = userid
-        self.userkey = userkey
+        self.user_id = user_id
+        self.user_key = user_key
 
-    def get_media_info(self, action='GetMediaInfo', ids=[], headers=ENCODING_API_HEADERS):
-        fields = { 'userid':self.userid,
-                   'userkey':self.userkey,
-                   'action':action,
-                   'mediaid':','.join(ids),
-                 }
+    def get_media_info(self, action='GetMediaInfo', ids=None, headers=ENCODING_API_HEADERS):
+        """
 
-        dq = {}
+        :param action:
+        :param ids:
+        :param headers:
+        :return:
+        """
+        if not ids:
+            ids = []
+
+        fields = {'userid': self.user_id,
+                  'userkey': self.user_key,
+                  'action': action,
+                  'mediaid': ','.join(ids)}
+
+        dq = dict()
         dq['query'] = fields
-        query = json.dumps( dq )
+        query = json.dumps(dq)
 
         results = self._execute_request(query, headers)
         return results
 
-    def get_status(self, action='GetStatus', ids=[], extended='no', headers=ENCODING_API_HEADERS):
-        fields = { 'userid':self.userid,
-                   'userkey':self.userkey,
-                   'action':action,
-                   'extended':extended,
-                   'mediaid':','.join(ids),
-                 }
+    def get_status(self, action='GetStatus', ids=None, extended='no', headers=ENCODING_API_HEADERS):
+        """
 
-        dq = {}
+        :param action:
+        :param ids:
+        :param extended:
+        :param headers:
+        :return:
+        """
+        if not ids:
+            ids = []
+
+        fields = {'userid': self.user_id,
+                  'userkey': self.user_key,
+                  'action': action,
+                  'extended': extended,
+                  'mediaid': ','.join(ids)}
+
+        dq = dict()
         dq['query'] = fields
-        query = json.dumps( dq )
+        query = json.dumps(dq)
 
         results = self._execute_request(query, headers)
         return results
 
-    def add_media(self, action='AddMedia', source=[], notify='', notify_format='', formats=[], instant='no', headers=ENCODING_API_HEADERS):
-        fields = { 'userid':self.userid,
-                   'userkey':self.userkey,
-                   'action':action,
-                   'source':source,
-                   'notify':notify,
-                   'notify_format':notify_format,
-                   'instant':instant,
-                   'format':formats
-                 }
+    def add_media(self, action='AddMedia', source=None, notify='', notify_format='', formats=None,
+                  instant='no', headers=ENCODING_API_HEADERS):
+        """
 
-        dq = {}
+        :param action:
+        :param source:
+        :param notify:
+        :param notify_format:
+        :param formats:
+        :param instant:
+        :param headers:
+        :return:
+        """
+        if not source:
+            source = []
+        if not formats:
+            formats = []
+
+        fields = {'userid': self.user_id,
+                  'userkey': self.user_key,
+                  'action': action,
+                  'source': source,
+                  'notify': notify,
+                  'notify_format': notify_format,
+                  'instant': instant,
+                  'format': formats}
+
+        dq = dict()
         dq['query'] = fields
-        query = json.dumps( dq )
+        query = json.dumps(dq)
 
         results = self._execute_request(query, headers)
         return results
 
     def _execute_request(self, json, headers, path='', method='POST'):
+        """
+
+        :param json:
+        :param headers:
+        :param path:
+        :param method:
+        :return:
+        """
         # print('json: {0}'.format(json) )
         params = urllib.parse.urlencode({'json': json})
 
         conn = http.client.HTTPConnection(self.url)
         conn.request(method, path, params, headers)
         response = conn.getresponse()
+        # TODO: Better handling of response, ie. errors
         data = response.read()
         conn.close()
 
