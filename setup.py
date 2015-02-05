b868cd6e28f6d1348aa56a9b4a792a5b880b74c9
David Hwu <dhwu@studionow.com>
2015-02-05 14:13:09 -0800

    Fixed pypi tarball variant, stuck with original non-PEP8 dash for project name for now.

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
 import os
 from setuptools import setup, find_packages
 
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(file_name):
     return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
-
-# TODO: Warning: way how we parse and provide read me files on the fly will on some systems result in a
-# theoretical race condition where the download folder will no longer exist once the read() has been invoked
-# parse this content before the setup() is called.  Removed dynamic processing of read me to provide description
-
 setup(
-    name="encodingcom_py3",
+    name="encodingcom-py3",
     version="0.0.5",
     description="Python 3 wrapper for Encoding.com api",
     license="",
