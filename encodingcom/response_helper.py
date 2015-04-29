cb60488a2494246b5fb31e110a3876e7aaf61acc
David Hwu <dhwu@studionow.com>
2015-04-29 13:10:08 -0700

    Format response helper retrieval

--- encodingcom/response_helper.py
+++ encodingcom/response_helper.py
@@ -23,9 +23,33 @@ def get_media_id(data: dict) -> str:
     """
     Retrieve encoding.com standard "mediaid" dict from the given data.
 
-    :param data: response dictionary from a call to encoding.com
+    :param data: dict
+        Entire response data returned by any of the calls to encoding.com
     :return: media_id matching the "MediaID" from a call to encoding.com, '' if not found
+    :rtype: str
+    """
+    response = get_response(data)
+    if response:
+        return response.get('MediaID', '')
+    else:
+        return {}
+
+
+def get_format(data: dict) -> dict:
+    """
+    Retrieve the format section in the response.
+
+    :param data: dict
+        Entire response data returned by any of the calls to encoding.com
+    :return:
+        dictionary representing all the format contents of the response
+        If there is no format in response found, empty dict is returned
     :rtype: dict
     """
     response = get_response(data)
-    return response.get('MediaID', '')
+    if response:
+        result = response.get('format')
+        if result:
+            return result
+
+    return {}
